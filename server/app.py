445feadd7c1dd1bc0fd856ed31e369f8aa801ff4
Ellen Bennett <bennette831@gmail.com>
2024-01-05 15:22:23 -0700

    add /auth route, and useEffect for getUser() in App.js, add auth resource in app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,17 @@
 from config import app, db, api
-from flask import request
+from flask_restful import Resource
+from flask import request, make_response, session
 from models import User, Location, Experience
+
+class Auth(Resource):
+    def get(self):
+        try:
+            user = User.query.filter_by(id=session['user_id']).first()
+            return make_response(user.to_dict(), 200)
+        except:
+            return(401, "Unauthorized")
+
+api.add_resource(Auth, '/auth')
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
