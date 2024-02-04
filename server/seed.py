e8ccc6ad47aca1915c8aedd1d5d88887c84aa73a
Ellen Bennett <bennette831@gmail.com>
2024-02-04 02:32:37 -0700

    change seed.py to accommodate new Rating

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,5 @@
 from config import app, db
-from models import User, Location, Experience
+from models import User, Location, Experience, Rating
 
 if __name__ == "__main__":
 
@@ -8,6 +8,7 @@ if __name__ == "__main__":
 		User.query.delete()
 		Location.query.delete()
 		Experience.query.delete()
+		Rating.query.delete()
 
 		u1 = User(username="jo shaw", email="jo@bellsouth.net", age="22")
 		u2 = User(username="shane doe", email="shane@yahoo.com", age="34")
@@ -17,8 +18,14 @@ if __name__ == "__main__":
 		l1 = Location(city="Wilmington", state="NC")
 		l2 = Location(city="Denver", state="CO")
 		db.session.add_all([l1, l2])
+		db.session.commit()
 
-		e1 = Experience(length="6 months", community=4, crowds=2, safety=3, user_id=u2.id, location_id=l1.id)
-		e2 = Experience(length="1 year", community=2, crowds=3, safety=1, user_id=u1.id, location_id=l2.id)
+		r1 = Rating(community=4, crowds=2, safety=3)
+		r2 = Rating(community=2, crowds=3, safety=1)
+		db.session.add_all([r1, r2])
+		db.session.commit()
+
+		e1 = Experience(length="6 months", rating_id=r1.id, user_id=u2.id, location_id=l1.id)
+		e2 = Experience(length="1 year", rating_id=r2.id, user_id=u1.id, location_id=l2.id)
 		db.session.add_all([e1, e2])
 		db.session.commit()
