61a6fb3c10c0156be7e18eb967bb92e607c84760
Ellen Bennett <bennette831@gmail.com>
2024-02-20 21:42:16 -0700

    add redux to packages, initialize store in main.jsx & provide as prop to Provider component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+
+const store = configureStore({
+  reducer: { }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 )
